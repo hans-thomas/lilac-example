43bf38e36235dc072d9b62bdfeea7ac2f13191aa
mortazavi <s.mohammad027@live.com>
2022-04-13 20:14:59 +0430

    menubar added;

---
+++ core/resources/views/sections/menubar.blade.php
@@ -0,0 +1,59 @@
+<section class="home1 home2 menubar">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="menu-bg">
+                    <div class="row align-items-center">
+                        <!-- main menu -->
+                        <div class="col-lg-12">
+                            <!-- desktop-menu -->
+                            <nav class="navbar position-static justify-content-center desktop-menu p-0">
+                                <div class="main-menu">
+                                    <ul class="menu">
+                                        <li>
+                                            <a href="#">خانه</a>
+                                        </li>
+                                        <li><a href="#">درباره ما</a></li>
+                                        <li><a href="#">صفحات</a></li>
+                                        <li class="logo">
+                                            <a href="#">
+                                                <img width="130px" height="130px"
+                                                     src="{{ asset('assets/images/home2/logo.png') }}"
+                                                     alt="Logo">
+                                            </a>
+                                        </li>
+                                        <li><a href="#">فروشگاه</a></li>
+                                        <li>
+                                            <a href="#">وبلاگ</a></li>
+                                        <li><a href="#">تماس با ما</a></li>
+                                    </ul>
+                                </div>
+                            </nav>
+                            <!-- mobile menu -->
+                            <nav class="navbar p-0 mobile-menu position-static">
+                                <div class="header-menu position-static">
+                                    <ul class="menu">
+                                        <li class="active">
+                                            <a href="#">خانه</a>
+                                        </li>
+                                        <li><a href="#">درباره</a></li>
+                                        <li>
+                                            <a href="#!">صفحات</a>
+                                        </li>
+                                        <li>
+                                            <a href="#!">فروشگاه</a>
+                                        </li>
+                                        <li>
+                                            <a href="#!">وبلاگ</a>
+                                        </li>
+                                        <li><a href="#">تماس با ما</a></li>
+                                    </ul>
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
