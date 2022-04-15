b4b078afb16cf6dc0f1530f442167e32fae4bcd5
mortazavi <s.mohammad027@live.com>
2022-04-15 16:26:02 +0430

    products lists created;

---
+++ core/resources/views/sections/scripts.blade.php
@@ -0,0 +1,35 @@
+<!-- jquery -->
+<script src="{{ asset('assets/plugins/jquery-3.4.1.min.js') }}"></script>
+
+<!-- bootstrap -->
+<script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+
+<!-- menu -->
+<script src="{{ asset('assets/plugins/menu.min.js') }}"></script>
+
+<!-- swiper slider -->
+<script src="{{ asset('assets/plugins/swiper-bundle.min.js') }}"></script>
+
+<!-- packery -->
+<script src="{{ asset('assets/plugins/packery.pkgd.min.js') }}"></script>
+
+<!-- countdown -->
+<script src="{{ asset('assets/plugins/jquery.countdown.min.js') }}"></script>
+
+<!-- venobox -->
+<script src="{{ asset('assets/plugins/venobox.min.js') }}"></script>
+
+<!-- jquery ui -->
+<script src="{{ asset('assets/plugins/jquery-ui.min.js') }}"></script>
+
+<!-- select -->
+<script src="{{ asset('assets/plugins/select2.min.js') }}"></script>
+
+<!-- zoom -->
+<script src="{{ asset('assets/plugins/jquery.elevateZoom-3.0.8.min.js') }}"></script>
+
+<!-- Google map -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjkssBA3hMeFtClgslO2clWFR6bRraGz0"></script>
+
+<!-- script -->
+<script src="{{ asset('assets/js/script.js') }}"></script>
